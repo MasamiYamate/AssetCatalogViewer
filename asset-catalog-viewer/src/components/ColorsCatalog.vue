9a9ee6979cc686040230cae8a20fe2117ec9dfd5
Masami <masami@MasaminoMacBook-Pro.local>
2020-09-02 22:46:31 +0900

    色の表示周り仮

--- asset-catalog-viewer/src/components/ColorsCatalog.vue
+++ asset-catalog-viewer/src/components/ColorsCatalog.vue
@@ -1,10 +1,27 @@
 <template>
   <v-app id="inspire">
-    <v-card class="mx-auto" min-width="300" bottom="50">
+    <v-card class="mx-auto" min-width="300" max-width="400" min-height="100" bottom="50">
       <v-container fluid>
         <v-row dense>
           <v-col v-for="(card, index) in colors" :key="index">
-            <v-card-title>{{card.name}}</v-card-title>
+            <v-avatar v-if="card.value.any" :color="card.value.any" size="120">
+              <div class="d-flex flex-column">
+              <span class="white--text headline">Light</span>
+              <span class="white--text headline">{{card.value.any}}</span>
+              </div>
+            </v-avatar>
+            <v-avatar color="indigo" size="120">
+              <div class="d-flex flex-column">
+              <span class="white--text headline">Light</span>
+              <span class="white--text headline">#000000</span>
+              </div>
+            </v-avatar>
+            <v-avatar color="indigo" size="120">
+              <div class="d-flex flex-column">
+              <span class="white--text headline">Light</span>
+              <span class="white--text headline">#000000</span>
+              </div>
+            </v-avatar>
           </v-col>
         </v-row>
       </v-container>
